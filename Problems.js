95ae404ef7b9ee5df300e94e02467677c6bef4be
AlifSakib <alifsakib@gmail.com>
2022-08-11 02:31:33 +0600

    JavaScript Program to Calculate the Area of a Triangle

---
+++ Problems.js
@@ -0,0 +1,26 @@
+// !1. JavaScript Program To Print Hello World
+
+console.log("Hello World");
+//document.write();
+//alert();
+
+// !2. JavaScript Program to Add Two Numbers
+
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+// !3. JavaScript Program to Find the Square Root
+
+// ?To find the square root of a number in JavaScript, you can use the built-in Math.sqrt() method.
+
+// ?If 0 or a positive number is passed in the Math.sqrt() method, then the square root of that number is returned.
+// ?If a negative number is passed, NaN is returned.
+// ?If a string is passed, NaN is returned.
+
+// !4. JavaScript Program to Calculate the Area of a Triangle
